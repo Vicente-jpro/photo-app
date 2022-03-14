2cd501205587d0ac548c7fea213b8ed78f60aad6
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-03-14 22:30:56 +0100

    create and edit

--- spec/controllers/properties_controller_spec.rb
+++ spec/controllers/properties_controller_spec.rb
@@ -52,7 +52,30 @@ RSpec.describe PropertiesController, type: :controller do
         it "should new post" do 
           get :new
           expect(assigns(:property)).to be_a(Property) 
+          #Same instruction
+          expect(assigns(:property)).to be_a_new(Property)
         end
     end
 
+
+    context "GET #edit" do 
+        let(:property) { create(:property) }
+        it "should success and render to edit page" do 
+          get :edit, params: {id: property.id}
+          expect(response).to have_http_status(200) 
+          expect(response).to render_template(:edit)
+        end
+    end
+
+
+    context "CREATE #create" do 
+        let!(:property) { create(:property)}
+        it "should create a new property" do 
+          set_property = { name: property.name, images: File.open(Rails.root.join('spec', 'factories', 'images', 'pacaça.jpeg'), filename: 'pacaça.jpeg', content_type: 'image/jpeg') }
+          post :create, params: { property: set_property  }
+          expect(flash[:notice]).to eq("Property was successfully created.")
+        end
+    end
+    
+    
 end
