d1ab010cb9d06bc2a85fd5ce3e3593f62e4052ca
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-03-15 00:42:07 +0100

    seed default image added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+(1..50).each do |number|
+    @property = Property.new
+    @property.name = "Nature #{number}"
+    @property.images.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'pacaça.jpeg')), filename: 'pacaça.jpeg', content_type: 'image/jpeg')
+    @property.save
+end
+
+#app/assets/images/pacaça.jpeg
